d0e0e118486846ac8eeacd24fd29061fcde9b55f
Patricio <pato.bengoechea@gmail.com>
2019-02-18 18:05:04 -0300

    ABMC Movie ok

--- app/models/movie.js
+++ app/models/movie.js
@@ -7,7 +7,9 @@ var movieSchema= new mongoose.Schema({
 	year: {type: Number, required: true},
 	genre: {type: String, required: true},
 	img_path: {type: String, required: false},
-  actors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'actor'}]
+  actors: [{ type: mongoose.Schema.Types.ObjectId, ref: 'actor'}],
+	release_date: {type: Date, required: false},
+	vote: {type: Number, required: false}
 }, {collection:'movie'});
 
 mongoose.model('movie', movieSchema);
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -15,8 +15,8 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
 
-    let id = req.params.id
-    movie.findById(id)
+    let id = req.params.id;
+    movie.find({"_id": ObjectId(id)})
       .populate('actors')
       .then(movies =>{
           if(!movies){ return res.sendStatus(401); }
@@ -29,7 +29,7 @@ router.post('/', (req, res, next) => {
     let name=req.body.name;
     let genre=req.body.genre;
     let year=req.body.year;
-    res.send("name:"+name+ ' ' + "genre:"+genre + ' '+ "year:"+year);
+
     let mo = new movie({
         name: req.body.name,
         genre: req.body.genre,
@@ -39,14 +39,66 @@ router.post('/', (req, res, next) => {
 
     mo.save()
         .then(doc => {
-            console.log(doc)
+            console.log(doc);
+            res.send("name:"+name+ ' ' + "genre:"+genre + ' '+ "year:"+year);
         })
     .catch(err => {
-        console.log(err)
+        console.log(err);
+        res.send("Ha ocurrido un error, intentelo de nuevo mas tarde");
     });
 
     next();
 });
+
+    router.delete('/:id', (req, res, next) => {
+        let id = req.params.id;
+        movie.findByIdAndRemove(id);
+        res.sendStatus(200);
+        }
+    );
+
+
+    router.put('/:id', (req, res, next) => {
+        movie.findById(id);
+        if (req.body.vote){
+            movie.vote++;
+        }
+        else{
+            movie.vote--;
+        }
+
+        movie.save()
+            .then(doc => {
+                console.log(doc);
+                res.send('Voto actualizado');
+            })
+            .catch(err => {
+                console.log(err);
+                res.send('Ha ocurrido un error, intentelo de nuevo mas tarde');
+            })
+    });
+
+    router.get('/movie/popular', (req, res, next) => {
+        movie.find({vote: {$gte: 10}})
+            .then(movies => {
+                if(!movies){return res.sendStatus(401);}
+                return res.json({'movies': movies})
+            })
+            .catch(next);
+    });
+
+    router.get('/movie/now-playing', (req, res, next) => {
+        let date2 = new Date();
+        date2.month=date2.getMonth()-1;
+        movie.find({release_date: {$gte: date2}})
+            .then(movies => {
+            if(!movies){return res.sendStatus(401);}
+            return res.json({'movies': movies})
+        })
+        .catch(next);
+    });
+
+
 
 
 module.exports=router;
